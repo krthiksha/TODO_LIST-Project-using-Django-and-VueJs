efda2a22b0d6c74d1ac640ec3d4da4354f051276
krthiksha <krithiksha21@gmail.com>
2025-05-02 16:12:42 +0530

    added dependencies in requirments.txt and modified config in settings.py

--- todo_drf_backend/todo_drf/settings.py
+++ todo_drf_backend/todo_drf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-5z4hjqudf44mk7^psa!_rqmvgn*o17!p@8bswi97t(kjb%(*n9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']     #Allows all Host
 
 
 # Application definition
@@ -59,6 +60,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # CORS headers
     'corsheaders.middleware.CorsMiddleware',
+    
+    # whitenoise configuration for serving static files 
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     
 ]
 
@@ -130,8 +134,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
