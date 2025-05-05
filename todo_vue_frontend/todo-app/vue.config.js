6d13a4ad98a38b414d28ecdf622267ef57b7cb77
krthiksha <krithiksha21@gmail.com>
2025-05-05 15:48:51 +0530

    added live app url in note file

--- todo_vue_frontend/todo-app/vue.config.js
+++ todo_vue_frontend/todo-app/vue.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  // configuration for production purpose
   publicPath: './',
   outputDir: 'dist'
 })
+
+
+// vercel.json --- file used to mention the standards of app to make the vercel platform understand better about the vueJs and its routing paths etc...
+// npm run build --- run this command to generate the build files (test it locally  to ensure it works efficiently -- as vercel generates this files automatically)
+
